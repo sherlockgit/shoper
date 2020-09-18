5ac682f5390fa1efc2d601cdf760efd608767002
linmuyu <linmuyu@champa.cn>
2020-09-18 20:12:44 +0800

    更新

--- sql/gbm_shop.sql
+++ sql/gbm_shop.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50645
 File Encoding         : 65001
 
-Date: 2020-09-11 17:17:59
+Date: 2020-09-18 20:11:39
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,18 +20,18 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `db_keys`;
 CREATE TABLE `db_keys` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `key` varchar(255) DEFAULT NULL COMMENT '关键字',
   `site` varchar(255) DEFAULT NULL COMMENT '站点：1 台湾，2 菲律宾',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for db_product
 -- ----------------------------
 DROP TABLE IF EXISTS `db_product`;
 CREATE TABLE `db_product` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `keys_id` int(11) NOT NULL COMMENT '关键字id',
   `shop_id` varchar(255) DEFAULT NULL COMMENT '商店id',
   `search_id` int(11) DEFAULT NULL COMMENT '搜索id',
@@ -39,7 +39,7 @@ CREATE TABLE `db_product` (
   `cat_id` varchar(255) DEFAULT NULL COMMENT '分类id',
   `ads_id` int(11) DEFAULT NULL COMMENT '广告id',
   `label_ids` varchar(255) DEFAULT NULL COMMENT '标签id',
-  `campaignid` int(11) DEFAULT NULL COMMENT '活动id',
+  `campaign_id` int(11) DEFAULT NULL COMMENT '活动id',
   `product_name` varchar(255) DEFAULT NULL COMMENT '产品名称',
   `brand` varchar(255) DEFAULT NULL COMMENT '品牌',
   `price_min` int(11) DEFAULT NULL COMMENT '最低价格',
@@ -47,7 +47,7 @@ CREATE TABLE `db_product` (
   `ads_keyword` varchar(255) DEFAULT NULL COMMENT '广告关键字',
   `free_shipping` varchar(255) DEFAULT NULL COMMENT '是否免运',
   `is_preferred_plus_seller` varchar(255) DEFAULT NULL COMMENT '是否为优选卖家',
-  `rating_star` double(10,10) DEFAULT NULL COMMENT '评分',
+  `rating_star` double(7,6) DEFAULT NULL COMMENT '评分',
   `cmt_count` int(11) DEFAULT NULL COMMENT '评价数',
   `view_count` int(11) DEFAULT NULL COMMENT '观看数量',
   `liked_count` int(11) DEFAULT NULL COMMENT '喜欢数量',
@@ -59,19 +59,39 @@ CREATE TABLE `db_product` (
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `serach_time` datetime DEFAULT NULL COMMENT '搜索时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=752 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Table structure for db_search
 -- ----------------------------
 DROP TABLE IF EXISTS `db_search`;
 CREATE TABLE `db_search` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `key_id` int(11) DEFAULT NULL COMMENT '关键字id',
   `key` varchar(255) DEFAULT NULL,
+  `order_type` int(11) DEFAULT NULL COMMENT '排序方式：1 低到高 2. 高到低',
+  `search_type` int(11) DEFAULT NULL COMMENT '查询方式：1 综合排名；2 最新；3：最热销；4：价格',
   `total_count` int(11) DEFAULT NULL COMMENT '总条数',
   `total_ads_count` int(11) DEFAULT NULL COMMENT '广告条数',
-  `ori_totalCount` varchar(255) DEFAULT NULL COMMENT '排除广告条数',
+  `ori_totalCount` int(11) DEFAULT NULL COMMENT '排除广告条数',
   `suggestion_algorithm` int(11) DEFAULT NULL COMMENT '建议算法？？',
-  `create_time` datetime DEFAULT NULL
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Table structure for db_word
+-- ----------------------------
+DROP TABLE IF EXISTS `db_word`;
+CREATE TABLE `db_word` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `search_id` int(11) DEFAULT NULL,
+  `key` varchar(255) DEFAULT NULL COMMENT '关键字',
+  `word` varchar(255) DEFAULT NULL COMMENT '词语',
+  `num` int(11) DEFAULT NULL COMMENT '出现数量',
+  `sold` int(20) DEFAULT NULL COMMENT '销量',
+  `history_sold` int(11) DEFAULT NULL,
+  `view_count` int(11) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
